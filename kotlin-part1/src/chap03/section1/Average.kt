dd1d16abe154e15b9c3ee34f42b9d6a50388e27d
Yi Joon Choi <choi990406@gmail.com>
2020-12-30 22:04:07 +0900

    201230

--- kotlin-part1/src/chap03/section1/Average.kt
+++ kotlin-part1/src/chap03/section1/Average.kt
@@ -1,2 +1,17 @@
 package chap03.section1
 
+fun average(initial: Float = 0f, vararg numbers: Float):Double{
+    var result = 0f
+    for(num in numbers)
+    {
+        result += num
+    }
+    println("result: $result, numbers.size: ${numbers.size}")
+    val avg = result/numbers.size
+    return (avg+initial).toDouble()
+}
+
+fun main() {
+    val result = average(5f,100f,90f,80f)
+    println("avg result:$result")
+}
