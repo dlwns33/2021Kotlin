60c7d815beef76e73a4693e9e8efef379e3b2746
Yi Joon Choi <yijoonchoi@YJC-MacBookPro-2.local>
2020-12-30 17:34:01 +0900

    third commit

--- kotlin-part1/src/chap02/section2/MinMax.kt
+++ kotlin-part1/src/chap02/section2/MinMax.kt
@@ -1,9 +1,9 @@
 package chap02.section2
 
 fun main() {
-    println("Int: ${Byte.MIN_VALUE}~${Byte.MAX_VALUE}")
+    println("Byte: ${Byte.MIN_VALUE}~${Byte.MAX_VALUE}")
     println("Int: ${Int.MIN_VALUE}~${Int.MAX_VALUE}")
-    println("Int: ${Short.MIN_VALUE}~${Short.MAX_VALUE}")
-    println("Int: ${Long.MIN_VALUE}~${Long.MAX_VALUE}")
-    println("Int: ${Double.MIN_VALUE}~${Double.MAX_VALUE}")
+    println("Short: ${Short.MIN_VALUE}~${Short.MAX_VALUE}")
+    println("Long: ${Long.MIN_VALUE}~${Long.MAX_VALUE}")
+    println("Double: ${Double.MIN_VALUE}~${Double.MAX_VALUE}")
 }
