e31f08831d18c3b7e6bfb1fc899e09e5b6feeccf
Yi Joon Choi <choi990406@gmail.com>
2021-01-01 23:25:34 +0900

    210101

--- kotlin-part1/src/chap04/section2/FuncArgument.kt
+++ kotlin-part1/src/chap04/section2/FuncArgument.kt
@@ -14,5 +14,6 @@ fun main() {
     val result = sum(10,10)
     val result2 = mul(sum(10,5),10)
     val result3 = funcFunc(2,3)
+    //val result4 = funcFunc(3,3)
     println("result: $result, result2: $result2, result3: $result3")
 }
